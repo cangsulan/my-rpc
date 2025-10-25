a921d0cd6e64266e1c56cf9a56e743d87dc9ee30
cangsulan <3024179062@qq.com>
2025-10-25 22:15:15 +0800

    支持自定义SPI的机制，通过扫描文件来加载序列化器的类，并支持自定义序列化器

--- my-rpc-core/src/main/java/com/zhang/myrpc/serializer/SerializerFactory.java
+++ my-rpc-core/src/main/java/com/zhang/myrpc/serializer/SerializerFactory.java
@@ -1,5 +1,7 @@
 package com.zhang.myrpc.serializer;
 
+import com.zhang.myrpc.spi.SpiLoader;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,20 +10,25 @@ import java.util.Map;
  */
 public class SerializerFactory {
 
-    /**
-     * 序列化映射（用于实现单例）
-     */
-    private static final Map<String, Serializer> KEY_SERIALIZER_MAP = new HashMap<String, Serializer>() {{
-        put(SerializerKeys.JDK, new JdkSerializer());
-        put(SerializerKeys.JSON, new JsonSerializer());
-        put(SerializerKeys.KRYO, new KryoSerializer());
-        put(SerializerKeys.HESSIAN, new HessianSerializer());
-    }};
+    // 用静态代码块，进行初始化操作
+    static {
+        SpiLoader.load(Serializer.class);
+    }
+
+//    /**
+//     * 序列化映射（用于实现单例）
+//     */
+//    private static final Map<String, Serializer> KEY_SERIALIZER_MAP = new HashMap<String, Serializer>() {{
+//        put(SerializerKeys.JDK, new JdkSerializer());
+//        put(SerializerKeys.JSON, new JsonSerializer());
+//        put(SerializerKeys.KRYO, new KryoSerializer());
+//        put(SerializerKeys.HESSIAN, new HessianSerializer());
+//    }};
 
     /**
      * 默认序列化器
      */
-    private static final Serializer DEFAULT_SERIALIZER = KEY_SERIALIZER_MAP.get("jdk");
+    private static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
 
     /**
      * 获取实例
@@ -30,7 +37,7 @@ public class SerializerFactory {
      * @return
      */
     public static Serializer getInstance(String key) {
-        return KEY_SERIALIZER_MAP.getOrDefault(key, DEFAULT_SERIALIZER);
+        return SpiLoader.getInstance(Serializer.class, key);
     }
 
 }
